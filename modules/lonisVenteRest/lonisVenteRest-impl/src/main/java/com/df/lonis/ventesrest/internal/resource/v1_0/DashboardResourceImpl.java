4523a8b44b9540daef9b023e62624090cdbaaf55
yaeleudes <yaelahodan@gmail.com>
2026-04-30 17:10:37 +0000

    feat: Return des dstats du dashboard

--- modules/lonisVenteRest/lonisVenteRest-impl/src/main/java/com/df/lonis/ventesrest/internal/resource/v1_0/DashboardResourceImpl.java
+++ modules/lonisVenteRest/lonisVenteRest-impl/src/main/java/com/df/lonis/ventesrest/internal/resource/v1_0/DashboardResourceImpl.java
@@ -1,9 +1,17 @@
 package com.df.lonis.ventesrest.internal.resource.v1_0;
 
+import com.df.lonis.ventesrest.dto.v1_0.DashboardKpis;
 import com.df.lonis.ventesrest.resource.v1_0.DashboardResource;
 
+import com.df.lonis.ventesservice.model.ChiffreAffaires;
+import com.df.lonis.ventesservice.service.ChiffreAffairesLocalService;
+import com.df.lonis.ventesservice.service.ConcessionnaireLocalService;
+import com.df.lonis.ventesservice.service.TerminalLocalService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
+
+import java.util.List;
 
 /**
  * @author HP
@@ -14,4 +22,40 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class DashboardResourceImpl extends BaseDashboardResourceImpl {
 
+	@Override
+	public DashboardKpis getDashboardKpis(String periode) {
+
+		long totalConcessionnaire = 0L;
+		long totalTerminal = 0L;
+		long totalChiffreAffaire = 0L;
+		long totalPaiement = 0L;
+
+		List<ChiffreAffaires> cas = _chiffreAffairesLocalService.getChiffreAffaireses(-1, -1);
+
+		for (ChiffreAffaires chiffreAffaire : cas) {
+			totalChiffreAffaire += chiffreAffaire.getBalance();
+			totalPaiement += chiffreAffaire.getPaiement();
+		}
+
+
+		DashboardKpis dashboardKpis = new DashboardKpis();
+
+		dashboardKpis.setTotalConcessionnaires((long) _concessionnaireLocalService.getConcessionnairesCount());
+		dashboardKpis.setTotalPointsDeVente((long) _terminalLocalService.getTerminalsCount());
+		dashboardKpis.setPaiementsTotal(totalPaiement);
+		dashboardKpis.setChiffreAffairesTotal(totalChiffreAffaire);
+
+
+
+		return dashboardKpis;
+	}
+
+	@Reference
+	private ConcessionnaireLocalService _concessionnaireLocalService;
+
+	@Reference
+	private TerminalLocalService _terminalLocalService;
+
+	@Reference
+	private ChiffreAffairesLocalService _chiffreAffairesLocalService;
 }
